c74cc94a4282992feba37b3115153cf534d7419c
Gabriel Moncau <gabriel.moncau@hotmail.com>
2020-02-17 11:44:43 -0500

    Project files
    Copy of 3D Animation 2019-2020 project

---
+++ SciVis 2019-2020/Assets/Scripts/SpaceshipScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpaceshipScript : MonoBehaviour
+{
+    public Transform newParent;
+    public float waitTime;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(WaitForParentChange(waitTime));
+    }
+
+    // Update is called once per frame
+    IEnumerator WaitForParentChange(float time)
+    {
+        yield return new WaitForSeconds(time);
+        transform.parent = newParent;
+    }
+}
